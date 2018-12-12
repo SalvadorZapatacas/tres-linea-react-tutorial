6a10a22ce06dfa82044d790ff5296d6dd5ef7e74
Salvador Zapata Casales <salvazapata@outlook.com>
2018-12-12 21:59:36 +0100

    Updates

---
+++ precache-manifest.7cb2c7051f624a59d23958b341a621e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "72ec355ab06b61743523",
+    "url": "/tres-linea-react-tutorial/static/js/runtime~main.72ec355a.js"
+  },
+  {
+    "revision": "c1c4037f72b9f2e0a4c8",
+    "url": "/tres-linea-react-tutorial/static/js/main.c1c4037f.chunk.js"
+  },
+  {
+    "revision": "5bc363a705c279ce0d67",
+    "url": "/tres-linea-react-tutorial/static/js/1.5bc363a7.chunk.js"
+  },
+  {
+    "revision": "c1c4037f72b9f2e0a4c8",
+    "url": "/tres-linea-react-tutorial/static/css/main.fd589d03.chunk.css"
+  },
+  {
+    "revision": "79d46a011b396cc8556de9affea5d232",
+    "url": "/tres-linea-react-tutorial/index.html"
+  }
+];
